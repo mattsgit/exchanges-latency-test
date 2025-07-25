331eef72f72832b4c34ff90ae6eb18bede968eb0
Matt <matt@liquidlabs.inc>
2025-07-25 15:35:29 -0400

    Fix favicon with custom HTML template

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [solid({ ssr: false })],
   base: '/exchanges-latency-test/', // GitHub Pages base path - change to '/' when custom domain is active
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
